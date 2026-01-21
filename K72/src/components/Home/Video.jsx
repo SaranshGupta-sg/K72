9cf060ab13ed194a40b7156d6926d62c57dcbc54
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-01-21 22:38:34 +0530

    ddd

--- K72/src/components/Home/Video.jsx
+++ K72/src/components/Home/Video.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
 const Video = () => {
   return (
-    <div>
-      Video
+    <div className='h-full w-full'>
+      <video
+        className='h-full w-full object-cover'
+        autoPlay
+        loop
+        muted
+        src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1769012773~exp=1769016373~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=1a4e138201c76ab60cfc7e2ef6daacf54eae1f5cfb398faa9f2c227667728aa3&r=dXMtZWFzdDE%3D"
+      />
     </div>
   )
 }
-
 export default Video
--- K72/src/pages/Home.jsx
+++ K72/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Video from '../components/Home/Video.jsx'
+import HomeHeroText from '../components/Home/HomeHeroText.jsx'
+import HomeBottomText from '../components/Home/HomeBottomText.jsx'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <div className='h-screen w-screen fixed'>
+        <Video />
+      </div>
+      <div className='h-screen w-screen relative flex flex-col'>
+        <HomeHeroText/>
+        <HomeBottomText/>
+      </div>
     </div>
   )
 }

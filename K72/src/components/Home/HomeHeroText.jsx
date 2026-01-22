46420589d19cdf482da1accd39d982bc54d53164
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-01-22 22:33:28 +0530

    adds

--- K72/src/components/Home/HomeHeroText.jsx
+++ K72/src/components/Home/HomeHeroText.jsx
@@ -4,7 +4,7 @@ import Video from './Video'
 const HomeHeroText = () => {
   return (<div className='font-[font1] pt-5 text-center'>
     <div className='text-[9vw] justify-center flex items-center uppercase leading-[8vw]'>L'étincelle</div>
-    <div className='text-[9vw] justify-center flex items-start uppercase leading-[8vw]'>qui <div className='h-[6.7vw] rounded-3xl overflow-hidden'><Video /></div> génère</div>
+    <div className='text-[9vw] justify-center flex items-start uppercase leading-[8vw]'>qui <div className='h-[6.7vw] rounded-full overflow-hidden'><Video /></div> génère</div>
     <div className='text-[9vw] justify-center flex items-center uppercase leading-[8vw]'>la créativité</div> </div>)
 }
 export default HomeHeroText
--- K72/src/components/Home/Video.jsx
+++ K72/src/components/Home/Video.jsx
@@ -7,7 +7,7 @@ const Video = () => {
         autoPlay
         loop
         muted
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1769016806~exp=1769020406~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=f464cb0df718377eb20985a8f26ee12649fe32ef1cbdfede2137cb1c135d7155&r=dXMtZWFzdDE%3D"
+        src='/Video/69496b2d.mp4'
       />
     </div>
   )
